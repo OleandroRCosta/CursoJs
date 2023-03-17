f57f080c44d23f7a1fa0e7eaf498b14543e8c375
OleandroRCosta <leandro_rc7@yahoo.com.br>
2023-03-17 11:51:38 -0300

    atualização

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+vel = 59.5
+console.log('A velocidade do seu carro é de ' + vel + 'km/h')
+if (vel > 60) {
+    console.log('Você foi multado por dirigir acima da velocidade permitida.')
+}
+console.log('Dirija com cuidade e sempre use o cinto de segurança!')
